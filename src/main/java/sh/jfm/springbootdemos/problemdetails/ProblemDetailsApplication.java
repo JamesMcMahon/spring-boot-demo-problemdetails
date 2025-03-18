95c32a2b62bfbea8dc63fee96b9c70fb7aabfdf9
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-03-18 20:11:28 +0000

    Better class names

--- src/main/java/sh/jfm/springbootdemos/problemdetails/ProblemDetailsApplication.java
+++ src/main/java/sh/jfm/springbootdemos/problemdetails/ProblemDetailsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProblemdetailsApplication {
+public class ProblemDetailsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProblemdetailsApplication.class, args);
+		SpringApplication.run(ProblemDetailsApplication.class, args);
 	}
 
 }
